6beebf8c368024c13f3e91f5ee02b9c76de64ec8
Mohammad Ali Mashta <m.mashta@tu-braunschweig.de>
2026-04-07 12:36:10 +0200

    initial commit

---
+++ src/test/java/calisthenics/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package calisthenics.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
